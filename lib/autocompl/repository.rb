a55986e7266be98789e0f48bbe30bdb4deced8df
timakin <seiji.takahashi@translimit.co.jp>
2017-01-25 18:53:40 +0900

    <modify> remove code from repository

--- lib/autocompl/repository.rb
+++ lib/autocompl/repository.rb
@@ -1,26 +1,12 @@
 module Autocompl
     module Repository
-        def get_autocomplete_items(parameters)
-            model = parameters[:model]
-            table_name = parameters[:table_name]
-            term    = parameters[:term]
-            method  = parameters[:method]
-            options = parameters[:options]
-            scopes  = Array(options[:scopes])
-            where   = options[:where]
-            limit   = get_autocomplete_limit(options)
-            order   = get_autocomplete_order(method, options, model)
-
-            items = model.all
-
-            scopes.each { |scope| items = items.send(scope) } unless scopes.empty?
-
-            items = items.select(get_autocomplete_select_clause(model, method, options)) unless options[:full_model]
-            items = items.where(get_autocomplete_where_clause(model, term, method, options))
-                         .limit(limit).order(order)
-            items = items.where(where) unless where.blank?
-
-            items.to_a
+        def get_collection(model, columns)
+            klass = model.to_s.camelize.constantize
+            records = []
+            for column in columns
+                records.concat(klass.select(column).distinct.map(&column))
+            end
+            records
         end
     end
 end
