75b36c600de65e48652a20d0fc954ed9b8cb4409
timakin <timaki.st@gmail.com>
2017-01-27 08:49:45 +0900

    [fix] postgresql

--- lib/autocompl/repository.rb
+++ lib/autocompl/repository.rb
@@ -4,9 +4,19 @@ module Autocompl
       klass = model.to_s.camelize.constantize
       items = []
       for column in columns
-        items.concat(klass.select(column).where("#{column} LIKE ?", "%#{options[:term]}%").limit(options[:limit]).distinct.map(&column))
+        items.concat(klass.select(column).where("#{column} #{like_clause(klass)} ?", "%#{options[:term]}%").limit(options[:limit]).distinct.map(&column))
       end
       items
+    end
+
+    private
+
+    def like_clause(model)
+      postgres?(model) ? 'ILIKE' : 'LIKE'
+    end
+
+    def postgres?(model)
+      model.connection.class.to_s.match(/PostgreSQLAdapter/)
     end
   end
 end
